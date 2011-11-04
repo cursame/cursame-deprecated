648dee38f4b20178d8d05aeff60755b2e3899b77
Mike Ruby <emikebr@gmail.com>
2011-11-04 02:47:15 -0600

    More UI changes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,12 @@
 module ApplicationHelper
   
-  def course_members
+  def course_members(model)
+    numbers = model.users.count
+    members = numbers == 1 ? t('courses.members.member') : t('courses.members.members')
+    result = "#{numbers} #{members}"
+  end
+  
+  def avatar
     
   end
   
--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -10,6 +10,10 @@ class AvatarUploader < CarrierWave::Uploader::Base
   version :small do
     process :resize_to_fit => [180, 180]
   end
+  
+  version :xsmall do
+    process :resize_to_fit => [40, 40]
+  end
 
   def extension_white_list
     %w(jpg jpeg gif png)
