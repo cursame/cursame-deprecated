826c419959cae73d49429175c9be3d55e92fed72
Jorge Gajon <gajon@gajon.org>
2011-12-14 17:46:54 -0600

    Show correct number of users in a course

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,5 @@
 module ApplicationHelper
-  
-  def course_members(model)
-    numbers = model.students.count
-    members = numbers == 1 ? t('courses.members.member') : t('courses.members.members')
-    result = "#{numbers} #{members}"
-  end
-  
+
   def avatar(user, size=nil)
     case size
       when :small
