4e1e37420743414a269ed6462906462447943249
Mike Ruby <emikebr@gmail.com>
2011-12-13 22:21:59 -0600

    Supervisor config form redirects back to form

--- app/controllers/supervisor/networks_controller.rb
+++ app/controllers/supervisor/networks_controller.rb
@@ -9,7 +9,7 @@ class Supervisor::NetworksController < ApplicationController
   def update
     @network = current_network
     if @network.update_attributes params[:network]
-      redirect_to supervisor_dashboard_path, :notice => t('flash.network_updated')
+      redirect_to edit_supervisor_network_path, :notice => t('flash.network_updated')
     else
       render :edit
     end
