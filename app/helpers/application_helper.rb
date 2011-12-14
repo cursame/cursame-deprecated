b0e6e53a4ef016092d3037e04191fe85690cd864
Jorge Gajon <gajon@gajon.org>
2011-12-14 17:31:48 -0600

    Show fallback avatar in other places.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,11 +8,11 @@ module ApplicationHelper
   
   def avatar(user, size=nil)
     case size
-      when 180
+      when :small
         # FIXME: We need a fallback image for supervisors.
         fallback = user.teacher? ? 'profesor.png' : 'alumno.png'
         image_tag(user.avatar_file.blank? ? fallback : user.avatar_file.small.url)
-      when 40
+      when :xsmall
         fallback = user.teacher? ? 'profesor_small.png' : 'alumno_small.png'
         image_tag(user.avatar_file.blank?  ? fallback : user.avatar_file.xsmall.url)
     end
