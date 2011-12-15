cd7f3c14d8b049626ee62e7185f3bdf0b478d56e
Jorge Gajon <gajon@gajon.org>
2011-12-14 23:54:15 -0600

    [#22210729] BUGFIX: Videomotion filter missing wmode
    https://www.pivotaltracker.com/story/show/22210729

--- config/initializers/auto_html.rb
+++ config/initializers/auto_html.rb
@@ -5,3 +5,12 @@ AutoHtml.add_filter(:image_with_link).with({:alt => ''}) do |text, options|
     "<a href=\"#{ERB::Util.html_escape match}\" target=\"_blank\">#{img}</a>"
   end
 end
+
+
+AutoHtml.add_filter(:dailymotion_with_wmode).with(:width => 480, :height => 360) do |text, options|
+  text.gsub(/http:\/\/www\.dailymotion\.com.*\/video\/(.+)_*/) do
+    video_id = $1
+    %{<object type="application/x-shockwave-flash" data="http://www.dailymotion.com/swf/#{video_id}&related=0" width="#{options[:width]}" height="#{options[:height]}"><param name="wmode" value="transparent"></param><param name="movie" value="http://www.dailymotion.com/swf/#{video_id}&related=0"></param><param name="allowFullScreen" value="true"></param><param name="allowScriptAccess" value="always"></param><a href="http://www.dailymotion.com/video/#{video_id}?embed=1"><img src="http://www.dailymotion.com/thumbnail/video/#{video_id}" width="#{options[:width]}" height="#{options[:height]}"/></a></object>}
+  end
+end
+
