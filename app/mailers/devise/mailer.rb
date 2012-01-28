f113742ddb5d0e41c51992b7120829351a3e9e4e
Miguel Cervera <miguelcerverac@gmail.com>
2012-01-27 21:45:10 -0600

    trying a fix to this

--- app/mailers/devise/mailer.rb
+++ app/mailers/devise/mailer.rb
@@ -2,7 +2,8 @@ class Devise::Mailer < ::ActionMailer::Base
   include Devise::Mailers::Helpers
 
   def confirmation_instructions(record)
-    devise_mail(record, :confirmation_instructions)
+    #devise_mail(record, :confirmation_instructions)
+    devise_mail(record, :reset_password_instructions)
   end
 
   def reset_password_instructions(record)
