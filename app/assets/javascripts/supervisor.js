c9fcce5259b95471bab0d96723acf63e90c2e305
Miguel Cervera <miguelcerverac@gmail.com>
2012-02-17 17:46:42 -0600

    as supervisor i can change the role of a teacher or student

--- app/assets/javascripts/supervisor.js
+++ app/assets/javascripts/supervisor.js
@@ -5,4 +5,10 @@ $(document).ready(function(){
   
   // Tabs for pending and approved teachers
   $('.tabs').tabs()
+  
+  //Show edit_user_role
+  $('.change_role_button').click(function(){
+    $(this).parent().next().show(400);
+    $(this).parent().hide(400);
+  });
 });
