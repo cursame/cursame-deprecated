52de612820b2b586fd88a84b01f0107cbadbf604
Miguel Cervera <miguelcerverac@gmail.com>
2012-03-07 15:57:06 -0600

    the test for the previous features

--- spec/acceptance/supervisors/manage_networks_spec.rb
+++ spec/acceptance/supervisors/manage_networks_spec.rb
@@ -22,4 +22,19 @@ feature 'Supervisor', %q{
     Network.should exist_with :slogan => 'Errare human est', :welcome_message => 'Bienvenida!!', :time_zone => 'Guadalajara'
     page.should have_notice I18n.t('flash.network_updated')
   end
+  
+  scenario 'disabling public registration' do
+    visit edit_supervisor_network_url :subdomain => @network.subdomain
+    uncheck 'network[public_registry]'
+    click_button 'submit'
+    
+    Network.should exist_with :public_registry => false
+    page.should have_notice I18n.t('flash.network_updated')
+    
+    sign_out :subdomain => @network.subdomain
+    page.should_not have_css '.register-box'
+    visit new_teacher_user_registration_url :subdomain => @network.subdomain
+    page.should_not have_css '.register-box'
+    
+  end
 end
