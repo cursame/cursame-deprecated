d08a9250f713b3518188d6e5a14b4ea254e7b87c
Armando <iam@armando.mx>
2012-04-12 13:05:58 -0500

    cambio de url para deploy en heroku

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -1,6 +1,6 @@
 Cursame::Application.configure do
   HOST = 'labbbs.net'
-  HEROKU_HOST = 'stark-mist-6540.herokuapp.com'
+  HEROKU_HOST = 'young-samurai-3345.herokuapp.com'
   config.cache_classes = true
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
