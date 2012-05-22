4066aadd9cf1f78b4a438956aca0fab6ac61d49a
José Alfredo Reyes de Antuñano <jose_alfredo@cursa.me>
2012-05-21 19:24:35 -0500

    solucionando problema de falle siguiendo routes se creo faye.ru se modificao layout/application.html.erb helpers/aplication.rb  assets/javascripts/aplication.js para la parte de rutas de  seguimiento de faye en heroku

--- Procfile
+++ Procfile
@@ -3,4 +3,4 @@ worker: bundle exec rake jobs:work
 beanstalk: beanstalkdrails
 faye:      rackup faye.ru -s thin -E production
 private_pub: rackup private_pub.ru -s thin -E production
-private_pub_production: rackup private_pub.ru -s thin -p $PORT -e $RACK_ENV
+private_pub_production: rackup private_pub.ru -s thin  -p $PORT -e $RACK_ENV
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,5 +4,9 @@ module ApplicationHelper
       "active"
     end
   end
-  
+  def broadcast(channel, &block)
+      message = {:channel => channel, :data => capture(&block), :ext => {:auth_token => FAYE_TOKEN}}
+      uri = URI.parse("http://fayeredis.herokuapp.com/faye")
+      Net::HTTP.post_form(uri, :message => message.to_json)
+    end
 end
