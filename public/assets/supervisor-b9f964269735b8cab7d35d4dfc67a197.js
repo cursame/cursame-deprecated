17b191067829cc00562ff93a923eb92a49b594cc
manduks <iam@armando.mx>
2012-06-22 14:30:26 -0500

    compilacion de assets

---
+++ public/assets/supervisor-b9f964269735b8cab7d35d4dfc67a197.js
@@ -0,0 +1,4 @@
+/*
+ Javascripts for Supervisor layout.
+*/
+$(document).ready(function(){$(".tabs").tabs(),$(".change_role_button").click(function(){$(this).parent().next().show(400),$(this).parent().hide(400)})});
