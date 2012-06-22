feb33675a5085defaf4ca2b946b3984fc2174cb3
manduks <iam@armando.mx>
2012-06-22 13:10:05 -0500

    se cambio la propiedad par aque los assets se compilen cuando la apllciaction carga

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -7,7 +7,7 @@ Cursame::Application.configure do
   config.action_controller.perform_caching = true
   config.serve_static_assets = false
   config.assets.compress = true
-  config.assets.compile = false
+  config.assets.compile = true
   config.assets.digest = true
 
   # Defaults to Rails.root.join("public/assets")
