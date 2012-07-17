862dafe43a6464ef1258cf4ebb7732111bd11f69
jose armando gonzalez lopez <jose.armando.gonzalez@gmail.com>
2012-07-16 19:52:22 -0500

    cambio de nombre de los migrates

--- db/migrate/20120711221048_create_favorites.rb
+++ db/migrate/20120711221048_create_favorites.rb
@@ -1,16 +1,10 @@
 class CreateFavorites < ActiveRecord::Migration
   def change
-    create_table :favorites do |t|
+    create_table :favorites_users do |t|
       t.integer :user_id
       t.integer :user_favorite_id
 
       t.timestamps
     end
-  end
-  def self.up
-          rename_table :favorites, :favorites_users
-      end 
-  def self.down
-          rename_table :favorites, :favorites_users
   end
 end
--- db/migrate/20120713184233_add_avatar_to_favorite.rb
+++ db/migrate/20120713184233_add_avatar_to_favorite.rb
@@ -1,5 +1,5 @@
 class AddAvatarToFavorite < ActiveRecord::Migration
   def change
-    add_column :favorites, :avatar, :string
+    add_column :favorites_users, :avatar, :string
   end
 end
