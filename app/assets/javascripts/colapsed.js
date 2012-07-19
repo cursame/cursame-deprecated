72f47ef5337e2f3cb515f827274558d128821327
José Alfredo Reyes de Antuñano <jose_alfredo@cursa.me>
2012-07-19 15:49:58 -0500

    colapsed an video png

---
+++ app/assets/javascripts/colapsed.js
@@ -0,0 +1,11 @@
+/* Click para aparecer desplegable*/
+
+
+$(function()
+{
+$(".desplegable-tutoriales").click(function(event) {
+event.preventDefault();
+$(".desplegable-interno").animate({height:'toggle'},150);
+return false;
+});
+});
