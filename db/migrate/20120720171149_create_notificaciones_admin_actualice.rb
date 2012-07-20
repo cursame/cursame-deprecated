4c4cda31c1f089d2c0612b8309c6af55e65d6dbd
José Alfredo Reyes de Antuñano <jose_alfredo@cursa.me>
2012-07-20 17:46:28 -0500

    subiendo default de notificaciones

--- db/migrate/20120720171149_create_notificaciones_admin_actualice.rb
+++ db/migrate/20120720171149_create_notificaciones_admin_actualice.rb
@@ -1,9 +1,9 @@
 class CreateNotificacionesAdminActualice < ActiveRecord::Migration
   def change
     create_table :notificaciones_admin_actualice do |t|
-      t.string :title
-      t.string :content
-      t.string :link_video
+      t.string :title, :default => "¡¡Ahora notificaciones del administrador!!"
+      t.string :content, :default => "Ahora puedes recibir notificaciones del administrador para estar siempre enterado de las últicas actualizaciones de Cúrsame"
+      t.string :link_video, :default => "link"
 
       t.timestamps
     end
