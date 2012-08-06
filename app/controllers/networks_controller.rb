f1e3775b65a5ef1a828c3b649bd192d652d247d8
José Alfredo Reyes de Antuñano <jose_alfredo@cursa.me>
2012-08-05 22:52:19 -0500

    subiendo estilos

--- app/controllers/networks_controller.rb
+++ app/controllers/networks_controller.rb
@@ -1,8 +1,8 @@
 class NetworksController < ApplicationController
   skip_before_filter :authenticate_active_user_within_network!, :only => [:network_cc , :create, :update, :instrucciones]
   #bloquea el layout en el network_cc
-  layout 'application', :except => [:network_cc ] 
-  layout 'new_network', :except => [:create, :update, :instrucciones, :principal_wall]
+ # layout 'application', :except => [:network_cc ] 
+ # layout 'new_network', :except => [:create, :update, :instrucciones, :principal_wall]
       def network_cc
         @network = Network.new
         @network.supervisors.build
