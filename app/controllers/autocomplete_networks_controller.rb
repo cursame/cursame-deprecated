e971430610f20dfaa398ffbb5b41ff8f9203f068
Luis Alfredo Lorenzo <wichobabas@gmail.com>
2012-08-13 21:14:50 -0500

    Autocomplete para redes

---
+++ app/controllers/autocomplete_networks_controller.rb
@@ -0,0 +1,16 @@
+class AutocompleteNetworksController < ApplicationController
+
+  skip_before_filter :authenticate_active_user_within_network!, :only => [:index, :terms, :network_cc]
+
+  def index
+    @networks_hash = []
+    @networks = Network.find :all
+
+    @networks.each do |n|
+       @networks_hash << { "label" => n.name }
+     end
+ 
+     render :json => @networks_hash
+  end
+
+end
