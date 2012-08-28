9ab8ae4698e4154cd7a5e3892b18f7ed93ac57b4
José Alfredo Reyes de Antuñano <jose_alfredo@cursa.me>
2012-08-28 15:41:53 -0500

    subiendo authentication token

--- db/migrate/20111031191504_devise_create_users.rb
+++ db/migrate/20111031191504_devise_create_users.rb
@@ -5,7 +5,7 @@ class DeviseCreateUsers < ActiveRecord::Migration
       ## Database authenticatable
         t.string :email,              :null => false, :default => ""
         t.string :encrypted_password, :null => false, :default => ""
-
+        t.string :authentication_token
         ## Recoverable
         t.string   :reset_password_token
         t.datetime :reset_password_sent_at
@@ -35,12 +35,12 @@ class DeviseCreateUsers < ActiveRecord::Migration
         # t.datetime :locked_at
 
         # Token authenticatable
-        t.string :authentication_token
+        # t.string :authentication_token
 
         ## Invitable
         # t.string :invitation_token
-
-        t.timestamps
+       
+        
     end
 
     add_index :users, :email,                :unique => true
