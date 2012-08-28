edfc141feae4281343df00273556d65fab8fb30c
José Alfredo Reyes de Antuñano <jose_alfredo@cursa.me>
2012-08-28 15:16:19 -0500

    subir confirmatiosn34

--- db/migrate/20120820174040_add_dn_to_user.rb
+++
@@ -1,5 +0,0 @@
-class AddDnToUser < ActiveRecord::Migration
-  def change
-    add_column :users, :dn, :integer
-  end
-end
--- db/migrate/20120820174259_add_di_to_user.rb
+++
@@ -1,5 +0,0 @@
-class AddDiToUser < ActiveRecord::Migration
-  def change
-    add_column :users, :di, :string
-  end
-end
