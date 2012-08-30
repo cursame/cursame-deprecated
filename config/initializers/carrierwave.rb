ec256cbbae76cc56bceeeb0700672c069c4a6284
José Alfredo Reyes de Antuñano <jose_alfredo@cursa.me>
2012-08-30 11:35:30 -0500

    agregando ruta directa de amazon a produccion

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,7 +1,8 @@
 CarrierWave.configure do |config|
   config.s3_access_key_id     = "AKIAISIGOKLLQ6T5H3NQ" 
   config.s3_secret_access_key = "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" 
-  config.s3_bucket            = "cursame-#{Rails.env}"
+  #config.s3_bucket            = "cursame-#{Rails.env}"
+  config.s3_bucket            = "cursame-production"
   config.storage(Rails.env.test? || Rails.env.development? ? :file : :s3) 
   config.s3_headers = {"Content-Disposition" => "attachment"}
 end
