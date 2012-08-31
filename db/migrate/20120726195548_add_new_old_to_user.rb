a9cbe8156e4df93b9061cfbb7786ded0fec399db
José Alfredo Reyes de Antuñano <jose_alfredo@cursa.me>
2012-08-30 20:07:39 -0500

    bajando schema

--- db/migrate/20120726195548_add_new_old_to_user.rb
+++ db/migrate/20120726195548_add_new_old_to_user.rb
@@ -1,5 +1,5 @@
 class AddNewOldToUser < ActiveRecord::Migration
   def change
-    add_column :users, :new_old, :integer, :default => 1
+    add_column :users, :new_old, :integer, :default => 0
   end
 end
