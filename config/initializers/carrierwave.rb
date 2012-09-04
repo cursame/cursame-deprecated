5413491b39ab67dc72e2291209023398b1ffe9d6
José Alfredo Reyes de Antuñano <jose_alfredo@cursa.me>
2012-09-04 14:06:15 -0500

    subiendo comentario en carrierwave

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,3 +1,4 @@
+#configuración de carrierwave
 CarrierWave.configure do |config|
   config.s3_access_key_id     = "AKIAISIGOKLLQ6T5H3NQ" 
   config.s3_secret_access_key = "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" 
