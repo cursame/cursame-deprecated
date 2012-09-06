92d50a5e98717eb7f78f021107d387dcc3806c9f
José Alfredo Reyes de Antuñano <jose_alfredo@cursa.me>
2012-09-05 22:22:13 -0500

    subiendo cambio de send mailer

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -25,7 +25,8 @@ class Asset < ActiveRecord::Base
       user.confirmed_at = DateTime.now
       if user.save
         #UserMailer.delay.new_user_by_supervisor(user, network, password)
-        UserMailer.new_user_by_supervisor(user, network, password)
+        #UserMailer.new_user_by_supervisor(user, network, password)
+        Devise::Mailer.reset_password_instructions(user, network, password)
       else
         puts user.errors.full_messages
         invalid_users << user
