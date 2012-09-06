037ce678436b8b869492257cf6e70eff1f79c783
José Alfredo Reyes de Antuñano <jose_alfredo@cursa.me>
2012-09-06 17:57:28 -0500

    User#accepting_email defaults to true

--- db/schema.rb
+++ db/schema.rb
@@ -58,13 +58,13 @@ ActiveRecord::Schema.define(:version => 20120906054022) do
   end
 
   create_table "bug_answers", :force => true do |t|
-    t.text      "container"
-    t.date      "date"
-    t.timestamp "created_at"
-    t.timestamp "updated_at"
-    t.integer   "send_report_id"
-    t.string    "send_report_status"
-    t.integer   "user_id"
+    t.text     "container"
+    t.date     "date"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "send_report_id"
+    t.string   "send_report_status"
+    t.integer  "user_id"
   end
 
   create_table "calendar_activities", :force => true do |t|
@@ -120,18 +120,18 @@ ActiveRecord::Schema.define(:version => 20120906054022) do
   end
 
   create_table "courses", :force => true do |t|
-    t.string    "name"
-    t.text      "description"
-    t.date      "start_date"
-    t.date      "finish_date"
-    t.boolean   "public"
-    t.string    "reference"
-    t.timestamp "created_at"
-    t.timestamp "updated_at"
-    t.integer   "network_id"
-    t.string    "course_logo_file"
-    t.integer   "chat_id"
-    t.string    "status"
+    t.string   "name"
+    t.text     "description"
+    t.date     "start_date"
+    t.date     "finish_date"
+    t.boolean  "public"
+    t.string   "reference"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "network_id"
+    t.string   "course_logo_file"
+    t.integer  "chat_id"
+    t.string   "status"
   end
 
   add_index "courses", ["network_id"], :name => "index_courses_on_network_id"
@@ -186,12 +186,12 @@ ActiveRecord::Schema.define(:version => 20120906054022) do
   add_index "enrollments", ["user_id"], :name => "index_enrollments_on_user_id"
 
   create_table "favorites", :force => true do |t|
-    t.integer   "user_id"
-    t.integer   "user_favorite_id"
-    t.timestamp "created_at"
-    t.timestamp "updated_at"
-    t.string    "avatar"
-    t.string    "name"
+    t.integer  "user_id"
+    t.integer  "user_favorite_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "avatar"
+    t.string   "name"
   end
 
   create_table "like_not_likes", :force => true do |t|
@@ -203,19 +203,19 @@ ActiveRecord::Schema.define(:version => 20120906054022) do
   end
 
   create_table "networks", :force => true do |t|
-    t.string    "subdomain"
-    t.string    "name"
-    t.timestamp "created_at"
-    t.timestamp "updated_at"
-    t.string    "slogan"
-    t.text      "welcome_message"
-    t.string    "logo_file"
-    t.string    "time_zone"
-    t.boolean   "public_registry",  :default => true
-    t.boolean   "private_registry", :default => false
-    t.string    "registry_domain"
-    t.string    "lenguajes"
-    t.string    "variante",         :default => "free"
+    t.string   "subdomain"
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "slogan"
+    t.text     "welcome_message"
+    t.string   "logo_file"
+    t.string   "time_zone"
+    t.boolean  "public_registry",  :default => true
+    t.boolean  "private_registry", :default => false
+    t.string   "registry_domain"
+    t.string   "lenguajes"
+    t.string   "variante",         :default => "free"
   end
 
   create_table "networks_users", :id => false, :force => true do |t|
@@ -227,18 +227,18 @@ ActiveRecord::Schema.define(:version => 20120906054022) do
   add_index "networks_users", ["user_id"], :name => "index_networks_users_on_user_id"
 
   create_table "new_users_change_types", :force => true do |t|
-    t.integer   "new_old"
-    t.integer   "user_id"
-    t.timestamp "created_at"
-    t.timestamp "updated_at"
+    t.integer  "new_old"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "notificaciones_admin_actualice", :force => true do |t|
-    t.string    "title",      :default => "Ahora notificaciones del administrador"
-    t.string    "content",    :default => "Ahora puedes recibir notificaciones del administrador para estar siempre enterado de las actualizaciones del sistema"
-    t.string    "link_video", :default => "link"
-    t.timestamp "created_at"
-    t.timestamp "updated_at"
+    t.string   "title",      :default => "Ahora notificaciones del administrador"
+    t.string   "content",    :default => "Ahora puedes recibir notificaciones del administrador para estar siempre enterado de las actualizaciones del sistema"
+    t.string   "link_video", :default => "link"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "notifications", :force => true do |t|
@@ -262,30 +262,30 @@ ActiveRecord::Schema.define(:version => 20120906054022) do
   end
 
   create_table "send_reports", :force => true do |t|
-    t.string    "title"
-    t.text      "text"
-    t.timestamp "event_date"
-    t.timestamp "created_at"
-    t.timestamp "updated_at"
-    t.integer   "user_id"
-    t.string    "typebug"
-    t.string    "status"
+    t.string   "title"
+    t.text     "text"
+    t.datetime "event_date"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "user_id"
+    t.string   "typebug"
+    t.string   "status"
   end
 
   create_table "status_courses", :force => true do |t|
-    t.string    "status"
-    t.date      "date"
-    t.integer   "course_id"
-    t.timestamp "created_at"
-    t.timestamp "updated_at"
+    t.string   "status"
+    t.date     "date"
+    t.integer  "course_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "statuses", :force => true do |t|
-    t.string    "view_status"
-    t.date      "date_status"
-    t.timestamp "created_at"
-    t.timestamp "updated_at"
-    t.integer   "user_id"
+    t.string   "view_status"
+    t.date     "date_status"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "survey_answers", :force => true do |t|
@@ -317,12 +317,12 @@ ActiveRecord::Schema.define(:version => 20120906054022) do
   end
 
   create_table "tutoriales", :force => true do |t|
-    t.string    "title"
-    t.text      "description"
-    t.date      "date"
-    t.string    "link"
-    t.timestamp "created_at"
-    t.timestamp "updated_at"
+    t.string   "title"
+    t.text     "description"
+    t.date     "date"
+    t.string   "link"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "users", :force => true do |t|
