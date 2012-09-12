c5c9d7114025f1c4872e5938c5455605c64db1bd
Rodrigo Ayala <azul.rejected@gmail.com>
2012-09-12 11:44:57 -0500

    Se Intenta romper pruebas

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -9,7 +9,7 @@ CarrierWave.configure do |config|
 end
 
 CarrierWave.configure do |config|
-  config.root      = Rails.root.join('tmp')
+  config.root      = Rails.root.join('tmpo')
   config.cache_dir = 'uploads/cached-carrierwave'
 end
 
