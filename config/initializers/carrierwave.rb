46c480e3d936ce60b234c602a5d73a9c17417913
Rodrigo Ayala <azul.rejected@gmail.com>
2012-09-12 12:06:06 -0500

    ...

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,14 +2,14 @@
 CarrierWave.configure do |config|
   config.s3_access_key_id     = "AKIAISIGOKLLQ6T5H3NQ" 
   config.s3_secret_access_key = "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" 
-  config.s3_bucket            = "cursame-#{Rails.env}"
-  #config.s3_bucket            = "cursame-staging"
+  #config.s3_bucket            = "cursame-#{Rails.env}"
+  config.s3_bucket            = "cursame-stagin"
   config.storage(Rails.env.test? || Rails.env.development? ? :file : :s3) 
   config.s3_headers = {"Content-Disposition" => "attachment"}
 end
 
 CarrierWave.configure do |config|
-  config.root      = Rails.root.join('tmpo')
+  config.root      = Rails.root.join('tmp')
   config.cache_dir = 'uploads/cached-carrierwave'
 end
 
