7e53c7045e602bee8de5db9567a371f3ddd2b905
José Alfredo Reyes de Antuñano <jose_alfredo@cursa.me>
2012-09-16 15:51:44 -0500

    subiendo modificiaciones al uploader de publicidad

--- app/uploaders/banner_image_uploader.rb
+++ app/uploaders/banner_image_uploader.rb
@@ -4,12 +4,14 @@ class BannerImageUploader < CarrierWave::Uploader::Base
 
   #include CarrierWave::MiniMagick
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  def store_dir
+    "uploads/banner_image/#{model.id}"
+  end
   
    
   # storage :fog
   def default_url 
-     "/assets/#{model.title}_#{version_name}.png"
+     "/assets/#{model.title}_banner_image.png"
    end
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
