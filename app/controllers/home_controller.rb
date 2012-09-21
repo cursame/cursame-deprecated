2661a4ea24cbc565af365976eb6294b2f84d80c8
José Alfredo Reyes de Antuñano <jose_alfredo@cursa.me>
2012-09-20 20:00:13 -0500

    subiendo ruta para settings

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -87,24 +87,4 @@ end
     username == "logosetvites" && password == "cursameadministratore54321ap09854321"
   end
 end
-
-def password
-  # I'm not using if current_user.update_attributes(params[:user])
-  # so that the admin can update his password. This would fail because
-  # the role of the admin is not allowed by the User model.
-
-  password      = params[:user][:password]
-  password_conf = params[:user][:password_confirmation]
-
-  if !password.blank? && password == password_conf
-    current_user.password              = password
-    current_user.password_confirmation = password
-    current_user.save(:validate => false)
-
-    redirect_to root_path, :notice => "Tu contraseña fue cambiada, por favor ingresa con tu nueva contraseña"
-  else
-    render :show
-  end
-end
-
 end
