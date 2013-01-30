a519b556445c2c639eaf928aefdd0e5d353cd4c2
jose armando gonzalez lopez <iam@armando.mx>
2013-01-29 20:30:11 -0600

    se corrigio el analitics para movistar, se le agrego un filtro sexy

--- app/assets/javascripts/supervisor.js
+++ app/assets/javascripts/supervisor.js
@@ -1,14 +1,18 @@
 /*
  Javascripts for Supervisor layout.
-*/
-$(document).ready(function(){
-  
+ */
+ $(document).ready(function(){
+
   // Tabs for pending and approved teachers
-  $('.tabs').tabs()
+  if(! $('.tabs') ){
+  	$('.tabs').tabs()
+ 	return false;
+  }
   
   //Show edit_user_role
   $('.change_role_button').click(function(){
-    $(this).parent().next().show(400);
-    $(this).parent().hide(400);
+  	$(this).parent().next().show(400);
+  	$(this).parent().hide(400);
   });
+
 });
