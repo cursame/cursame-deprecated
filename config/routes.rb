595c6fc63479ffb5bf75ae100750609a647b54c0
Luis Alfredo Lorenzo <wichobabas@gmail.com>
2013-03-26 19:31:42 -0600

    Se corrige colision en ruta

--- config/routes.rb
+++ config/routes.rb
@@ -201,7 +201,7 @@ Cursame::Application.routes.draw do
   match '/api/api/create_comment', :to => 'api/api#create_comment', :as => :create_comment
   match '/api/api/create_like', :to => 'api/api#create_like', :as => :create_like
   match '/api/api/questions', :to => 'api/api#questions', :as => :questions
-  match '/api/api/survey_replies', :to => 'api/api#survey_replies', :as => :survey_replies
+  match '/api/api/survey_replies', :to => 'api/api#survey_replies'
 
   # top users
   match '/api/api/topusers', :to => 'api/api#topusers', :as => :topusersjson
