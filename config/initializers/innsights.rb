d77855fc8e8b90be22116e5e8848afa4c2abaf99
Emiliano <jemiliano.cabrera@gmail.com>
2013-09-11 00:00:32 -0500

    Ya se puede inicializar en un ambiente desde 0

--- config/initializers/innsights.rb
+++ config/initializers/innsights.rb
@@ -1,44 +1,46 @@
 # Extensive Docs at http://github.com/innku/innsights-gem
+=begin
 Rails.configuration.to_prepare do
   Innsights.setup do
     user User do
       display :name
       group :network
     end
-  
+
     group Network do
       display :name
     end
-  
+
     watch Comment do
       report lambda { |comment| "comment_#{comment.commentable.class.to_s.downcase}" }
       user :user
     end
-    
+
     watch Course do
       report "new_course"
       user :owner
     end
-    
+
     watch Enrollment do
       report lambda { |enrollment| "#{enrollment.role}_enrollment"}
       user :user
     end
-    
+
     watch User do
       report lambda  { |user| "#{user.role}_registered"}
       # group :network # En caso de que se quiera un ranking de registros por red
     end
-    
+
     watch SurveyReply do
       report "survey_reply"
       user :user
     end
-    
+
     watch Delivery do
       report "new_delivery"
       user :user
     end
-    
+
   end
 end
+=end
--- db/migrate/20111031191504_devise_create_users.rb
+++ db/migrate/20111031191504_devise_create_users.rb
@@ -24,10 +24,10 @@ class DeviseCreateUsers < ActiveRecord::Migration
         # t.string :password_salt
 
         ## Confirmable
-        # t.string   :confirmation_token
-        # t.datetime :confirmed_at
-        # t.datetime :confirmation_sent_at
-        # t.string   :unconfirmed_email # Only if using reconfirmable
+         t.string   :confirmation_token
+         t.datetime :confirmed_at
+         t.datetime :confirmation_sent_at
+         #t.string   :unconfirmed_email # Only if using reconfirmable
 
         ## Lockable
         # t.integer  :failed_attempts, :default => 0 # Only if lock strategy is :failed_attempts
@@ -35,17 +35,18 @@ class DeviseCreateUsers < ActiveRecord::Migration
         # t.datetime :locked_at
 
         # Token authenticatable
-        # t.string :authentication_token
+        #t.string :authentication_token
 
         ## Invitable
         # t.string :invitation_token
 
         t.timestamps
+        t.string :bussines_role
     end
 
     add_index :users, :email,                :unique => true
     add_index :users, :reset_password_token, :unique => true
-    # add_index :users, :confirmation_token,   :unique => true
+    add_index :users, :confirmation_token,   :unique => true
     # add_index :users, :unlock_token,         :unique => true
     # add_index :users, :authentication_token, :unique => true
   end
